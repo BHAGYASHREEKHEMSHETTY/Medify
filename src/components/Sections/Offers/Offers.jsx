1271d8852467f8a329f57176582a254ffd5577ef
BHAGYASHREEKHEMSHETTY <bhagynelge@gmail.com>
2025-11-12 17:29:20 +0530

    Your commit message

---
+++ src/components/Sections/Offers/Offers.jsx
@@ -0,0 +1,48 @@
+import {Container,Box} from "@mui/material";
+import {Autoplay,Pagination} from "swiper/modules";
+import {Swiper,SwiperSlide} from "swiper/react";
+import "swiper/css";
+import offer1 from "../../../assets/offer1.png";
+import offer2 from "../../../assets/offer2.png";
+export default function Offers(){
+    return (
+        <Box py={6}>
+            <Container maxWidth="xl">
+                <Swiper
+                  slidesPerView={1}
+                  spaceBetween={30}
+                  autoplay={{
+                    delay:3000,
+                    disableOnInteraction:false,
+                 }}
+                  modules={[Autoplay,Pagination]}
+                  pagination={{clickable:true}}
+                  breakpoints={
+                    {
+                        767:{slidesPerView:3}
+                    }
+                  }
+                >
+                    <SwiperSlide>
+                        <Box component={"img"} src={offer1} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Box component={"img"} src={offer2} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Box component={"img"} src={offer1} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Box component={"img"} src={offer2} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Box component={"img"} src={offer1} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Box component={"img"} src={offer2} />
+                    </SwiperSlide>
+                </Swiper>
+            </Container>
+        </Box>
+    )
+}
